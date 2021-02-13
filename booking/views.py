78d917fb9bd4d87216f83d24368c94f0fb71976c
korea3611 <korea3611@naver.com>
2021-02-13 19:44:29 +0900

    Initial commit

---
+++ booking/views.py
@@ -0,0 +1,18 @@
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from. models import Booking
+from .serializers import BookingSerializer
+from .permissions import IsOwnerOrReadOnly
+
+class BookingList(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,IsOwnerOrReadOnly)
+
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
